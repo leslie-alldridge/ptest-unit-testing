ec2ed00943f29a595f8ef1bceb75e460072e4bc7
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-06 06:41:41 +1300

    string tests

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Unit testing with Python
 - Unit testing is a software testing method by which individual units of source code are tested to determine whether they are fit for use.
 
 - Unittest is in the Python standard library. Nose and Pytest are not.
+
+- Can run tests with pytest or more verbose with pytest -v use specific files only by pytest filename
--- test_math_func.py
+++ test_math_func.py
@@ -9,3 +9,18 @@ def test_add():
 def test_product():
     assert math_func.product(5, 5) == 25
     assert math_func.product(5) == 10
+
+
+def test_add_string():
+    result = math_func.add('Hello', ' World')
+    assert result == 'Hello World'
+    assert type(result) is str
+    assert 'Heldo' not in result
+
+
+def test_product_strings():
+    assert math_func.product('Hello ', 3) == 'Hello Hello Hello '
+    result = math_func.product('Hello ')
+    assert result == 'Hello Hello '
+    assert type(result) is str
+    assert 'Hello' in result
